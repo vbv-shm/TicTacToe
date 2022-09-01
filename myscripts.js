8aadc621f3e968479334c0bf6183e4d6e0457e55
Vaibhav.8june@gmail.com <88285949+colorfulbutterflies@users.noreply.github.com>
2022-09-01 21:36:28 +0530

    Almost done.

---
+++ myscripts.js
@@ -0,0 +1,58 @@
+var xORo="X";
+function convert(){
+    if (xORo=="X"){
+        xORo="O";
+    }
+    else{
+        xORo="X";
+    }
+}
+document.getElementsByTagName('ca')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('cb')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('cc')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('cd')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('ce')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('cf')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('cg')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('ch')[0].addEventListener('click', addSymbol);
+document.getElementsByTagName('ci')[0].addEventListener('click', addSymbol);
+
+function addSymbol(){
+    if (this.innerText===""){this.innerText=xORo;convert();
+    console.log(this.tagName);
+    if(xORo=="X"){var s=3;}else {s=5};
+    if(this.tagName=='CA' ){score[0]=s;}
+    else if (this.tagName=='CB' ){score[1]=s;}
+    else if(this.tagName=='CC' ){score[2]=s;}
+    else if(this.tagName=='CD' ){score[3]=s;}
+    else if(this.tagName=='CE' ){score[4]=s;}
+    else if(this.tagName=='CF' ){score[5]=s;}
+    else if(this.tagName=='CG' ){score[6]=s;}
+    else if(this.tagName=='CH' ){score[7]=s;}
+    else if(this.tagName=='CI' ){score[8]=s;};
+    checkScore();
+}
+    
+function checkScore(){
+    if(score[0]+score[1]+score[2]==9||score[3]+score[4]+score[5]==9||score[6]+score[7]+score[8]==9||score[0]+score[3]+score[6]==9||score[1]+score[4]+score[7]==9||score[2]+score[5]+score[8]==9||score[0]+score[4]+score[8]==9||score[2]+score[4]+score[6]==9){ console.log("Player 1 won.")}
+    else if(score[0]+score[1]+score[2]==15||score[3]+score[4]+score[5]==15||score[6]+score[7]+score[8]==15||score[0]+score[3]+score[6]==15||score[1]+score[4]+score[7]==15||score[2]+score[5]+score[8]==15||score[0]+score[4]+score[8]==15||score[2]+score[4]+score[6]==15){console.log("Player 2 won.")}
+}
+}
+const player = (name) => {
+    const sayHello = () => console.log('hello!');
+    return { name, age, sayHello };
+  };
+function restart(){
+    document.getElementsByTagName('ca')[0].innerText="";
+    document.getElementsByTagName('cb')[0].innerText="";
+    document.getElementsByTagName('cc')[0].innerText="";
+    document.getElementsByTagName('cd')[0].innerText="";
+    document.getElementsByTagName('ce')[0].innerText="";
+    document.getElementsByTagName('cf')[0].innerText="";
+    document.getElementsByTagName('cg')[0].innerText="";
+    document.getElementsByTagName('ch')[0].innerText="";
+    document.getElementsByTagName('ci')[0].innerText="";
+    for(var i=0;i<9;i++){score[i]=0};
+}
+document.getElementsByTagName('button')[0].addEventListener('click', restart);
+var score=[0,0,0,0,0,0,0,0,0];
