13a4ee58847cd02dbfe960c6c405bf7ca658ec4c
Vaibhav.8june@gmail.com <88285949+colorfulbutterflies@users.noreply.github.com>
2023-02-17 17:38:48 +0530

    added hover effect on cells

--- myscripts.js
+++ myscripts.js
@@ -7,22 +7,25 @@ function convert(){
     else{
         xORo="X";
     }
+    document.getElementById("main").classList.toggle("mainx");
+    document.getElementById("main").classList.toggle("maino");
 }
 
 function addSymbol(){
-    if (this.innerText===""){this.innerText=xORo;convert();
-    
-    if(xORo=="X"){var s=3;}else {s=5};
-    if(this.tagName=='CA' ){score[0]=s;}
-    else if(this.tagName=='CB' ){score[1]=s;}
-    else if(this.tagName=='CC' ){score[2]=s;}
-    else if(this.tagName=='CD' ){score[3]=s;}
-    else if(this.tagName=='CE' ){score[4]=s;}
-    else if(this.tagName=='CF' ){score[5]=s;}
-    else if(this.tagName=='CG' ){score[6]=s;}
-    else if(this.tagName=='CH' ){score[7]=s;}
-    else if(this.tagName=='CI' ){score[8]=s;};
-    checkScore();
+    if (this.innerText===""){
+        this.classList.remove('block')
+        this.innerText=xORo;convert();
+        if(xORo=="X"){var s=3;}else {s=5};
+        if(this.tagName=='CA' ){score[0]=s;}
+        else if(this.tagName=='CB' ){score[1]=s;}
+        else if(this.tagName=='CC' ){score[2]=s;}
+        else if(this.tagName=='CD' ){score[3]=s;}
+        else if(this.tagName=='CE' ){score[4]=s;}
+        else if(this.tagName=='CF' ){score[5]=s;}
+        else if(this.tagName=='CG' ){score[6]=s;}
+        else if(this.tagName=='CH' ){score[7]=s;}
+        else if(this.tagName=='CI' ){score[8]=s;};
+        checkScore();
 }
 }
 
@@ -62,7 +65,10 @@ const player = (name) => {
     const sayHello = () => console.log('hello!');
     return { name, age, sayHello };
   };
+
+
 function restart(){
+
     document.getElementsByTagName('ca')[0].innerText="";
     document.getElementsByTagName('cb')[0].innerText="";
     document.getElementsByTagName('cc')[0].innerText="";
@@ -73,6 +79,15 @@ function restart(){
     document.getElementsByTagName('ch')[0].innerText="";
     document.getElementsByTagName('ci')[0].innerText="";
 
+    document.getElementsByTagName('ca')[0].classList.add('block');
+    document.getElementsByTagName('cb')[0].classList.add('block');
+    document.getElementsByTagName('cc')[0].classList.add('block');
+    document.getElementsByTagName('cd')[0].classList.add('block');
+    document.getElementsByTagName('ce')[0].classList.add('block');
+    document.getElementsByTagName('cf')[0].classList.add('block');
+    document.getElementsByTagName('cg')[0].classList.add('block');
+    document.getElementsByTagName('ch')[0].classList.add('block');
+    document.getElementsByTagName('ci')[0].classList.add('block');
     globalWinningID.forEach((winningId)=>{
         document.getElementById("block"+winningId).classList.remove("winningClass")
     })
@@ -81,6 +96,11 @@ function restart(){
     for(var i=0;i<9;i++){score[i]=0};
     document.getElementsByTagName('resultdata')[0].innerText="Game is on!";
     xORo="X";
+    document.getElementById("main").classList.contains("maino") && document.getElementById("main").classList.remove("maino")
+
+    document.getElementById("main").classList.add("mainx")
+
+
 }
 
 function colorWinningTiles(){
